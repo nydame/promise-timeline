6fcee52e4859d1bcd1e33c32a0e1a45008d63ab6
CodeFreq <codefreq@Macintosh-256.local>
2019-04-13 12:14:46 -0700

    Enhance testing abilities.

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {render, fireEvent, cleanup, waitForElement} from 'react-testing-library';
+import 'jest-dom/extend-expect';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
