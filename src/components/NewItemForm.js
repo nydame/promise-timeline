05f37afd6363f7cc234131ec9a71e2f4ad5eb585
CodeFreq <codefreq@Macintosh-256.local>
2019-04-15 03:05:36 -0700

    Try to add datepicker to form. (unsuccessful)

--- src/components/NewItemForm.js
+++ src/components/NewItemForm.js
@@ -1,9 +1,44 @@
 import React, {Component} from 'react';
 import {Divider} from '@material-ui/core';
+import Axios from 'axios';
 
-function NewItemForm(props) {
-    const display = props.hidden? "none" : "block";
-    return (<div className="new-item-form" style={{display}}>Form component here.<Divider /></div>);
+class NewItemForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            newTimelineItem: {
+                date: new Date().getTime(),
+                attendable: false,
+                attended: false,
+                clientId: null,
+                type: null,
+                event: null,
+                message: null
+
+            }
+        };
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(ev) {
+        // change state in response to user input
+    }
+
+    handleSubmit(ev) {
+        // use Axios to post to API
+        // remember to change date string to milliseconds
+    }
+
+    render() {
+        const display = this.props.hidden? "none" : "block";
+        return (<form className="new-item-form" style={{display}} onSubmit={this.handleSubmit}>
+            Use this form to add an event to the timeline
+            <Divider />
+            <label for="date-picker">Choose date and time of event</label>
+            <input type="datetime-local" id="date-picker" name="date-picker" value={new Date().toLocaleDateString()} />
+            <input type="submit" />
+        </form>);
+    }
 }
 
 export default NewItemForm;
